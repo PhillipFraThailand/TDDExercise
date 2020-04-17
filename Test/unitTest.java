4706149905ff1a9b9fab2da8ebe330790d12657a
stef2761 <Snookey1995@hotmail.com>
2020-04-17 12:56:57 +0200

    unit test working

---
+++ Test/unitTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class unitTest {
+    @Test
+    public void steffen() {
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+       steffen(1);
+    }
+    public static int steffen(int num) {
+
+       return num;
     }
 }
